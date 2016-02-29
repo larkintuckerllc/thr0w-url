6cdf6c9c5094bc51f2662f2a44e9138371159267
John Tucker <john@larkintuckerllc.com>
2016-02-29 13:27:03 -0500

    fix for multiple tabs

--- background.js
+++ background.js
@@ -1,27 +1,37 @@
 (function() {
   'use strict';
-    var chrome = window.chrome;
-    var thr0w = window.thr0w;
-    var base = localStorage.getItem("thr0w_url_base");
-    var username = localStorage.getItem("thr0w_url_username");
-    var password = localStorage.getItem("thr0w_url_password");
-    var channel = parseInt(localStorage.getItem("thr0w_url_channel"));
-    thr0w.setBase(base);
-    thr0w.login(username, password, login);
-    function login(error) {
-      if (error) {
-        throw 401;
-      }
-      thr0w.connect(channel, connect, message);
-      function connect() {
-      }
-      function message(data) {
+  var chrome = window.chrome;
+  var thr0w = window.thr0w;
+  var base = localStorage.getItem('thr0w_url_base');
+  var username = localStorage.getItem('thr0w_url_username');
+  var password = localStorage.getItem('thr0w_url_password');
+  var channel = parseInt(localStorage.getItem('thr0w_url_channel'));
+  thr0w.setBase(base);
+  thr0w.login(username, password, login);
+  function login(error) {
+    if (error) {
+      throw 401;
+    }
+    thr0w.connect(channel, connect, message);
+    function connect() {
+    }
+    function message(data) {
+      if (data.message.action === 'update') {
         var properties = {
-          url: data.message
+          url: data.message.url
         };
-        chrome.tabs.update(properties, callback);
-        function callback() {
+        chrome.tabs.query({}, handleQuery);
+      }
+      function handleQuery(tabs) {
+        var i;
+        for (i = 0; i < tabs.length; i++) {
+          if (i === 0) {
+            chrome.tabs.update(tabs[i].id, properties);
+          } else {
+            chrome.tabs.remove(tabs[i].id);
+          }
         }
+      }
     }
   }
 })();
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 (function() {
   'use strict';
   document.addEventListener('DOMContentLoaded', function() {
-    var base = localStorage.getItem("thr0w_url_base");
-    var username = localStorage.getItem("thr0w_url_username");
-    var password = localStorage.getItem("thr0w_url_password");
-    var channel = localStorage.getItem("thr0w_url_channel");
+    var base = localStorage.getItem('thr0w_url_base');
+    var username = localStorage.getItem('thr0w_url_username');
+    var password = localStorage.getItem('thr0w_url_password');
+    var channel = localStorage.getItem('thr0w_url_channel');
     var formEl = document.getElementById('form');
     var baseEl = document.getElementById('base');
     var usernameEl = document.getElementById('username');
@@ -24,10 +24,10 @@
         ) {
         return;
       }
-      localStorage.setItem("thr0w_url_base", baseEl.value);
-      localStorage.setItem("thr0w_url_username", usernameEl.value);
-      localStorage.setItem("thr0w_url_password", passwordEl.value); 
-      localStorage.setItem("thr0w_url_channel", channelEl.value);
+      localStorage.setItem('thr0w_url_base', baseEl.value);
+      localStorage.setItem('thr0w_url_username', usernameEl.value);
+      localStorage.setItem('thr0w_url_password', passwordEl.value);
+      localStorage.setItem('thr0w_url_channel', channelEl.value);
       window.close();
     }
   });
